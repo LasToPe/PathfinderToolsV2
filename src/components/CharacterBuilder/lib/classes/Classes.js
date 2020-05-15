085e463176e199328b90f3106f2dd707a6fc7940
Lasse Pedersen <lassep95@gmail.com>
2020-05-15 20:58:58 +0200

    Updated wizard to class

--- src/components/CharacterBuilder/lib/classes/Classes.js
+++ src/components/CharacterBuilder/lib/classes/Classes.js
@@ -8,6 +8,7 @@ import Paladin from './core/Paladin';
 import Ranger from './core/Ranger';
 import Rogue from './core/Rogue';
 import Sorcerer from './core/Sorcerer';
+import Wizard from './core/Wizard';
 
 export default {
     Barbarian: new Barbarian(),
@@ -20,5 +21,5 @@ export default {
     Ranger: new Ranger(),
     Rogue: new Rogue(),
     Sorcerer: new Sorcerer(),
-    Wizard: require('./core/Wizard.json')
+    Wizard: new Wizard()
 }
---
+++ src/components/CharacterBuilder/lib/classes/core/Wizard.js
@@ -0,0 +1,98 @@
+import Class from "../Class"
+import Special from "../Special";
+
+export default class Wizard extends Class {
+
+    constructor() {
+        super();
+
+        this.Name = "Wizard";
+        this.Description = "Beyond the veil of the mundane hide the secrets of absolute power. The works of beings beyond mortals, the legends of realms where gods and spirits tread, the lore of creations both wondrous and terrible—such mysteries call to those with the ambition and the intellect to rise above the common folk to grasp true might. Such is the path of the wizard. These shrewd magic-users seek, collect, and covet esoteric knowledge, drawing on cultic arts to work wonders beyond the abilities of mere mortals. While some might choose a particular field of magical study and become masters of such powers, others embrace versatility, reveling in the unbounded wonders of all magic. In either case, wizards prove a cunning and potent lot, capable of smiting their foes, empowering their allies, and shaping the world to their every desire.";
+        this.Role = "While universalist wizards might study to prepare themselves for any manner of danger, specialist wizards research schools of magic that make them exceptionally skilled within a specific focus. Yet no matter their specialty, all wizards are masters of the impossible and can aid their allies in overcoming any danger.";
+        this.Alignment = "Any";
+        this.HitDie = 6;
+        this.ClassSkills = [
+            "Appraise",
+            "Craft",
+            "Fly",
+            "Knowledge (all)",
+            "Linguistics",
+            "Profession",
+            "Spellcraft"
+        ];
+        this.SkillPointsPerLevel = 2;
+        this.BaseAttackBonus = this.HalfBaB;
+        this.Fortitude = this.BadSave;
+        this.Reflex = this.BadSave
+        this.Will = this.GoodSave
+        this.Specials = [
+            [
+                new Special("Arcane Bond", require('./Specials/WizardArcaneBonds.json')),
+                new Special("Arcane School", require('./Specials/WIzardArcaneSchools.json')),
+                new Special("cantrips"),
+                new Special("Scribe Scroll")
+            ],
+            [
+                this.Blank
+            ],
+            [
+                this.Blank
+            ],
+            [
+                this.Blank
+            ],
+            [
+                this.BonusFeat
+            ],
+            [
+                this.Blank
+            ],
+            [
+                this.Blank
+            ],
+            [
+                this.Blank
+            ],
+            [
+                this.Blank
+            ],
+            [
+                this.BonusFeat
+            ],
+            [
+                this.Blank
+            ],
+            [
+                this.Blank
+            ],
+            [
+                this.Blank
+            ],
+            [
+                this.Blank
+            ],
+            [
+                this.BonusFeat
+            ],
+            [
+                this.Blank
+            ],
+            [
+                this.Blank
+            ],
+            [
+                this.Blank
+            ],
+            [
+                this.Blank
+            ],
+            [
+                this.BonusFeat
+            ]
+        ]
+    }
+
+    get BonusFeat() {
+        return new Special("Bonus Feat");
+    }
+}
