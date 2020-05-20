36fa3729a3508c2e8bed660f1ceae84ed1852e89
Lasse Pedersen <lassep95@gmail.com>
2020-05-20 22:46:17 +0200

    Added remaining skills

--- src/components/CharacterBuilder/lib/Character.js
+++ src/components/CharacterBuilder/lib/Character.js
@@ -26,7 +26,28 @@ export default class Character {
         HandleAnimal: new Skill('Handle Animal', this.Abilities.Charisma),
         Heal: new Skill('Heal', this.Abilities.Wisdom),
         Intimidate: new Skill('Intimidate', this.Abilities.Charisma),
-        KnowledgeArcana: new Skill('Knowledge (arcana)', this.Abilities.Intelligence)
+        KnowledgeArcana: new Skill('Knowledge (arcana)', this.Abilities.Intelligence),
+        KnowledgeDungeoneering: new Skill('Knowledge (dungeoneering)', this.Abilities.Intelligence),
+        KnowledgeEngineering: new Skill('Knowledge (engineering)', this.Abilities.Intelligence),
+        KnowledgeGeography: new Skill('Knowledge (geography)', this.Abilities.Intelligence),
+        KnowledgeHistory: new Skill('Knowledge (history)', this.Abilities.Intelligence),
+        KnowledgeLocal: new Skill('Knowledge (local)', this.Abilities.Intelligence),
+        KnowledgeNature: new Skill('Knowledge (nature)', this.Abilities.Intelligence),
+        KnowledgeNobility: new Skill('Knowledge (nobility)', this.Abilities.Intelligence),
+        KnowledgePlanes: new Skill('Knowledge (planes)', this.Abilities.Intelligence),
+        KnowledgeReligion: new Skill('Knowledge (religion)', this.Abilities.Intelligence),
+        Linguistics: new Skill('Linguistics', this.Abilities.Intelligence),
+        Perception: new Skill('Perception', this.Abilities.Wisdom),
+        Perform: new Skill('Perform', this.Abilities.Charisma),
+        Profession: new Skill('Profession', this.Abilities.Wisdom),
+        Ride: new Skill('Ride', this.Abilities.Dexterity),
+        SenseMotive: new Skill('Sense Motive', this.Abilities.Wisdom),
+        SleightOfHand: new Skill('Sleight of Hand', this.Abilities.Dexterity),
+        Spellcraft: new Skill('Spellcraft', this.Abilities.Intelligence),
+        Stealth: new Skill('Stealth', this.Abilities.Dexterity),
+        Survival: new Skill('Survival', this.Abilities.Wisdom),
+        Swim: new Skill('Swim', this.Abilities.Strength),
+        UseMagicDevice: new Skill('Use Magic Device', this.Abilities.Charisma)
     };
 
 }
