96647a98b35eaefa9c21fa41dd8d9ec211402f25
Lasse Pedersen <lassep95@gmail.com>
2020-05-21 11:36:39 +0200

    Refactored Gnome

--- src/components/CharacterBuilder/lib/races/Races.js
+++ src/components/CharacterBuilder/lib/races/Races.js
@@ -1,10 +1,11 @@
 import Dwarf from './core/Dwarf';
 import Elf from './core/Elf';
+import Gnome from './core/Gnome';
 
 export default {
     Dwarf: new Dwarf(),
     Elf: new Elf(),
-    Gnome: require('./core/Gnome.json'),
+    Gnome: new Gnome(),
     HalfElf: require('./core/Half-Elf.json'),
     Halfling: require('./core/Halfling.json'),
     HalfOrc: require('./core/Half-Orc.json'),
