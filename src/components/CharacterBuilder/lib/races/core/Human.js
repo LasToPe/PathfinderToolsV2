1573149db4d9c075ed820ae4db49453ec96422a8
Lasse Pedersen <lassep95@gmail.com>
2020-05-21 11:47:21 +0200

    Human refactored

---
+++ src/components/CharacterBuilder/lib/races/core/Human.js
@@ -0,0 +1,23 @@
+import Race from "../Race"
+
+export default class Human extends Race {
+
+    constructor() {
+        super();
+
+        this.Name = "Human";
+        this.Description = "Ambitious, sometimes heroic, and always confident, humans have an ability to work together toward common goals that makes them a force to be reckoned with. Though short-lived compared to other races, their boundless energy and drive allow them to accomplish much in their brief lifetimes.";
+        this.AbilityMods = {
+            any: true
+        };
+        this.Size = "Medium";
+        this.RacialTraits = [
+            {
+                "Bonus Feat": "Humans select one extra feat at 1st level."
+            },
+            {
+                "Skilled": "Humans gain 1 additional skill rank at 1st level and 1 additional rank whenever they gain a level."
+            }
+        ];
+    }
+}
